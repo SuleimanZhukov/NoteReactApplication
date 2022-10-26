39bf6ac9a36e55c4cff7267a47e14f9b821446c3
Suleiman <suleimanzhukov@gmail.com>
2022-10-26 18:59:04 +0300

    trying to solve this problem

--- src/components/addSearch.jsx
+++ src/components/addSearch.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-const AddSearch = () => {
+const AddSearch = (props) => {
   return (
     <div className="search-form">
-      <input type="text" name="search" placeholder="Search Notes..." />
+      <input
+        className="input-search"
+        type="text"
+        name="search"
+        placeholder="Search Notes..."
+      />
       <button className="add-note-button">+ ADD NOTE</button>
     </div>
   );
